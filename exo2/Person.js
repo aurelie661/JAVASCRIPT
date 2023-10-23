0a1d2f1cc54e797484cf0bd1ed7e40d98064f8c3
aurelie661 <boutaurel59@gmail.com>
2023-10-23 16:29:05 +0200

    exo2 gestion du dom

---
+++ exo2/Person.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(firstName, lastName, email, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export default Person;
---
+++ exo2/script.mjs
@@ -0,0 +1,32 @@
+import Person from "./Person.js";
+
+const userForm = document.querySelector("#userForm");
+const userCount = document.querySelector("#userCount");
+const userList = document.querySelector("#userList");
+const users = [];
+
+userForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = document.querySelector("#firstName").value;
+  const lastName = document.querySelector("#lastName").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const person = new Person(firstName, lastName, email, password);
+  users.push(person);
+
+  updateUserList();
+  userCount.textContent = users.length;
+  console.log("Listing des Utilisateurs:", users);
+
+  userForm.reset();
+});
+
+function updateUserList() {
+  userList.innerHTML = "";
+  users.forEach((person) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Prénom: ${person.firstName}, Nom: ${person.lastName}, Email: ${person.email}`;
+    userList.appendChild(listItem);
+  });
+}
