60bafc5e33b7b582be5472d3ee0bd736e2d09ded
aurelie661 <boutaurel59@gmail.com>
2023-10-24 23:37:50 +0200

    exo4 done

--- exo4/assets/script/script.js
+++ exo4/assets/script/script.js
@@ -1,6 +1,21 @@
 const span1 = document.querySelector('#span1')
 const span2 = document.querySelector('#span2')
 
-function calculator(){
-    
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function evaluateExpression() {
+    try {
+        const expression = document.getElementById('display').value;
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Erreur';
+    }
 }
